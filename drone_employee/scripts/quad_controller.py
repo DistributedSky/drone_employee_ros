7109bde2e49f8867ec01e9fd1d2806b317bdaabd
Alexander Krupenkin <mail@akru.me>
2016-02-16 23:19:24 +0300

    Added route runner, quad_controller changed for multiple mission support.

--- drone_employee/scripts/quad_controller.py
+++ drone_employee/scripts/quad_controller.py
@@ -3,10 +3,11 @@
 from mission_wrapper import waypointWrap
 from quad_commander import *
 
-from rospy import init_node, spin, sleep, Subscriber, Publisher
+from rospy import init_node, is_shutdown, spin, sleep, logerr, loginfo
+from rospy import Subscriber, Publisher
 from small_atc_msgs.msg import *
 from sensor_msgs.msg import NavSatFix
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool, UInt32
 from threading import Thread
 from Queue import Queue
 
@@ -18,51 +19,57 @@ def take_current(queue):
     drop_all(queue)
     return queue.get(True, None)
 
-def quad_controller(target_queue, position_queue, arming_queue):
-    target = target_queue.get(True, None)
-    print('Received #'+str(target.id)+' mission')
-    if not target.valid:
-        print('Route invalid!')
-        return
-    # Write a mission
-    push_mission(waypointWrap(target.route))
-    print('Mission created')
-    # Set manual mode
-    set_mode('ACRO')
-    # Enable motors 
-    arming()
-    # Set autopilot mode
-    set_mode('AUTO')
-    print('Flight!')
-    # Wainting for arming
-    sleep(5)
-    drop_all(arming_queue)
-    while arming_queue.get().data:
-        pass
-    print('Mission complete')
+def quad_controller(route_queue, position_queue, arming_queue):
+    flight_done_pub = Publisher('release', UInt32, queue_size=1) 
+    while not is_shutdown():
+        # Take a route
+        route = route_queue.get(True, None)
+        loginfo('Received route with id #'+str(route.id)+'.')
+        if not route.valid:
+            logerr('Route invalid!')
+            return
+        # Push a mission into flight controller
+        push_mission(waypointWrap(target.route))
+        loginfo('Mission loaded to flight controller.')
+        # Set manual mode
+        set_mode('ACRO')
+        # Enable motors 
+        arming()
+        # Set autopilot mode
+        set_mode('AUTO')
+        loginfo('Flight!')
+
+        # Wainting for arming
+        # TODO: More elegant case for mission finish check
+        sleep(5)
+        drop_all(arming_queue)
+        while arming_queue.get().data:
+            pass
+
+        loginfo('Mission #'+str(route.id)+' complete!')
+        flight_done_pub.publish(route.id)
+        
 
 def main():
     ''' The main routine '''
     init_node('dron_employee_ros')
     # Create queues
-    target_queue = Queue()
+    route_queue = Queue()
     position_queue = Queue()
     arming_queue = Queue()
     # Create controller thread
     controller = Thread(target=quad_controller,
                         args=(target_queue, position_queue, arming_queue))
     
-    # Target message handler
-    def quad_target(msg):
+    # Route message handler
+    def quad_route(msg):
         if not controller.isAlive():
             controller.start()
-            target_queue.put(msg)
-        else:
-            print('Target already exist!')
+        route_queue.put(msg)
     
     Subscriber('mavros/global_position/global', NavSatFix, position_queue.put) 
     Subscriber('armed', Bool, arming_queue.put)
-    Subscriber('target', RouteResponse, quad_target)
+    Subscriber('route', RouteResponse, quad_route)
     spin()
             
 if __name__ == '__main__':
---
+++ drone_employee/scripts/run_route.js
@@ -0,0 +1,19 @@
+if (process.argv.length < 4) {
+    console.log("USAGE: node run_route.js 0x1234.... '[[65.32, 49.12, 15], ...'");
+    process.exit(1);
+}
+
+var address = process.argv[2];
+var points  = JSON.parse(process.argv[3]);
+var web3    = new require('web3')(); 
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+var drone_employee_abi = [{"constant":false,"inputs":[{"name":"_response","type":"address"}],"name":"setRoute","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"_route_id","type":"uint32"}],"name":"flightDone","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"subscribers","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[{"name":"latitude","type":"int256"},{"name":"longitude","type":"int256"},{"name":"altitude","type":"int256"}],"name":"addCheckpoint","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"checkpoints","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[],"name":"initROS","outputs":[],"type":"function"},{"constant":false,"inputs":[],"name":"takeFlight","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"_name","type":"string"},{"name":"_type","type":"string"}],"name":"mkPublisher","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[{"name":"_name","type":"string"},{"name":"_type","type":"string"},{"name":"_callback","type":"address"}],"name":"mkSubscriber","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"publishers","outputs":[{"name":"","type":"address"}],"type":"function"},{"inputs":[{"name":"_controller","type":"address"}],"type":"constructor"}];
+
+var contract = web3.eth.contract(drone_employee_abi).at(address);
+points.map(function(pt) {
+    contract.addCheckpoint.sendTransaction({from: web3.eth.accounts[0]},
+        pt[0] * 1000000,
+        pt[1] * 1000000,
+        pt[2] * 1000000);
+});
+contract.takeFlight.sendTransaction({from: web3.eth.accounts[0]});
